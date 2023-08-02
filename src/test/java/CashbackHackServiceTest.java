eebcc2b0582f75041ab6fd3ffe1d9208884ed8ee
aveDN <117298308+aveDN@users.noreply.github.com>
2023-08-02 23:41:35 +0300

    Add Junit4

---
+++ src/test/java/CashbackHackServiceTest.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.assertEquals;
+
+import ru.netology.CashbackHackService;
+
+import java.util.concurrent.Callable;
+
+public class CashbackHackServiceTest {
+    @org.junit.Test
+
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+    @org.junit.Test
+    public void testRemainWithCashback() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(expected, actual);
+    }
+
+}
